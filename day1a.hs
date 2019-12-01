edf95a2132f02f296891d14a430eed7c8398c504
Clint Hepner <clint.hepner@gmail.com>
2019-12-01 16:10:47 -0500

    Solved Day 1, Part 2

--- day1a.hs
+++ day1a.hs
@@ -4,7 +4,13 @@ fuelReq :: Int -> Int
 fuelReq m = m `div` 3  - 2
 
 
+realFuelReq :: Int -> Int
+realFuelReq m = let fuelMass = fuelReq m
+                    fuelMassReq = fuelReq fuelMass
+                in if fuelMassReq <= 0 then fuelMass else (fuelMass + realFuelReq fuelMass)
+
+
 
 main = do
     contents <- readFile "day1a.input"
-    print (sum $ map fuelReq $ map read $ lines contents)
+    print (sum $ map realFuelReq $ map read $ lines contents)
