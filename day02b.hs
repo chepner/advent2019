ef2a2ccc1501c39c13a2de4089111c96656bc154
Clint Hepner <clint.hepner@gmail.com>
2019-12-02 18:07:09 -0500

    Clean up some type usage; add some state-based experimental code

--- day02b.hs
+++ day02b.hs
@@ -2,13 +2,34 @@ import System.IO
 import Data.List.Split
 import qualified Data.Vector as V
 
-import Control.Monad.Trans.State
-import Debug.Trace
+--import Control.Monad.Trans.State
 
 type Program = V.Vector Int
 type PC = Int
 type Input = (Int, Int)
 type Result = (Input, Int)
+
+{-
+type ProgramState = State (Program, PC)
+
+evalStep :: ProgramState ()
+evalStep ps = do
+    (p, pc) <- ps
+    let opcode = p V.! pc
+    case opcode of
+      99 -> return ()
+      otherwise = let src1 = p V.! (pc + 1)
+                      src2 = p V.! (pc + 2)
+                      dest = p V.! (pc + 3)
+                      op1 = p V.! src1
+                      op2 = p V.! src2
+                      f = case opcode of
+                           1 -> (+)
+                           2 -> (*)
+                      p' = p V.// [(dest, f op1 op2)]
+                      pc' = pc + 4
+                    in put (p', pc')
+-}
 
 
 eval :: Program -> PC -> (Program, PC)
@@ -26,7 +47,7 @@ eval p pc = let opcode = p V.! pc
                               in eval (p V.// [(dest, f op1 op2)]) (pc + 4)
 
 
-getData :: FilePath -> IO (V.Vector Int)
+getData :: FilePath -> IO Program
 getData fname = do
     contents <- readFile fname
     return $ V.fromList $ map read $ splitOn "," contents
