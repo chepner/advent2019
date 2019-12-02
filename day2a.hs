3db378224467654472ff4a3ecfa6877d9f7c3075
Clint Hepner <clint.hepner@gmail.com>
2019-12-02 12:49:46 -0500

    Solved Day 2, Part 1

--- day2a.hs
+++ day2a.hs
@@ -1,5 +1,27 @@
 import System.IO
 import Data.List.Split
+import qualified Data.Vector as V
+
+import Control.Monad.Trans.State
+import Debug.Trace
+
+type Program = V.Vector Int
+type PC = Int
+
+
+eval :: Program -> PC -> (Program, PC)
+eval p pc = let opcode = p V.! pc
+            in case opcode of
+                 99 -> (p, pc)
+                 otherwise -> let src1 = p V.! (pc + 1)
+                                  src2 = p V.! (pc + 2)
+                                  dest = p V.! (pc + 3)
+                                  op1 = p V.! src1
+                                  op2 = p V.! src2
+                                  f = case opcode of
+                                       1 -> (+)
+                                       2 -> (*)
+                              in eval (p V.// [(dest, f op1 op2)]) (pc + 4)
 
 
 getData :: FilePath -> IO [Int]
@@ -7,13 +29,12 @@ getData fname = do
     contents <- readFile fname
     return $ map read $ splitOn "," contents
 
-evalProgram :: [Int] -> [Int]
-evalProgram = id
 
 main = do
   (p0:_:_:prest) <- getData "day2a.input"
   let mod_program = p0:12:2:prest
-  let (pos0:_) = evalProgram mod_program
-  print pos0
+      program = V.fromList mod_program
+      result = fst (eval program 0)
+  print $ result V.! 0  -- 3306701
   
   
