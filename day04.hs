ec7e099ac96220a789b785de153c2a399bebc457
Clint Hepner <clint.hepner@gmail.com>
2019-12-04 10:25:47 -0500

    Refactor predicates

--- day04.hs
+++ day04.hs
@@ -7,20 +7,23 @@ input = "145852-616942"
 generateOrderings :: [Char] -> [Ordering]
 generateOrderings = map (uncurry compare) . (zip <*> tail)
 
+hasEQ :: [Ordering] -> Bool
+hasEQ = elem EQ
+
+hasNoGT :: [Ordering] -> Bool
+hasNoGT = notElem GT
+
+hasLoneEQ :: [Ordering] -> Bool
+hasLoneEQ = elem [EQ] . group
+
 validate :: [Char] -> Bool
 validate l@[_,_,_,_,_,_] = let x = generateOrderings l
-                           in EQ `elem` x && GT `notElem` x
+                           in hasEQ x && hasNoGT x
 validate _ = False
-
-
-properEQ :: [Ordering] -> Bool
-properEQ xs = [EQ] `elem` group xs
 
 betterValidate :: [Char] -> Bool
 betterValidate l@[_,_,_,_,_,_] = let x = generateOrderings l
-                                 in EQ `elem` x
-                                    && GT `notElem` x
-                                    && properEQ x
+                                 in hasLoneEQ x && hasNoGT x
                       
 
 processInput :: String -> [Int]
@@ -29,5 +32,5 @@ processInput = map read . splitOn "-"
 main = do
   let low, high::Int
       [low, high] = processInput input
-  --print $ length $ filter (validate . show ) [low..high]  -- 1767
+  print $ length $ filter (validate . show ) [low..high]  -- 1767
   print $ length $ filter (betterValidate . show ) [low..high]  -- 1192
