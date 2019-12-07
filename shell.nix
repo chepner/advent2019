83c17986958ab1b1b6c56f0a9eb2cba922cd0a45
Clint Hepner <clint.hepner@gmail.com>
2019-12-07 10:48:50 -0500

    Add a comment on how it is and could be written

--- shell.nix
+++ shell.nix
@@ -1,3 +1,31 @@
+# For my own benefit...
+# Rather than a function with an argument
+# that defaults to import <nixpkgs> {}, this could have been
+# written as an expreesion like
+#
+#   let pkgs = import <nixpakgs> {};
+#   in pkgs.mkShell {
+#        buildInputs = [pkgs.ghcid
+#                      (pkgs.haskellPackages.ghcWithPackages (p: [
+#                          p.split
+#                          p.vector
+#                          p.containers
+#                      ]))];
+#
+#
+# or simply
+#
+#  with import <nixpkgs> {};
+#
+#  mkShell {
+#    buildInputs = [
+#      ghcid
+#      (haskellPackages.ghcWithPackages (p: [...]))
+#    ]
+#
+# In either case, the packages involved would have to come from
+# <nixpkgs>, with no option to override which set of packages to use.
+
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
   buildInputs = [pkgs.ghcid
