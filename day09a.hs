d16876f8d38be7769667edecf9f7a0d646f06193
Clint Hepner <clint.hepner@gmail.com>
2019-12-09 13:59:08 -0500

    Progress on Day 9, Part 1
    I'm getting an error on opcode 203, I think. Not sure
    what the problem is, though.
    
    I've implemented opcode 9, incremet relative base.
    I've implemented relative addressing.
    I've switche from Vector Int to IntMap Integer to handle
    both large integer values and to handle a larger address space.

--- day09a.hs
+++ day09a.hs
@@ -1,6 +1,6 @@
 import System.IO
 import Data.List.Split
-import qualified Data.Vector as V
+import Data.IntMap as IM
 import Debug.Trace
 import System.Environment
 import Data.Bool
@@ -8,13 +8,14 @@ import Data.Bool
 import Control.Monad.Trans.State
 import Data.List
 
-type Program = V.Vector Int
-type Addr = Int
-type PC = Int
-type RelBase = Int
-type Input = (Int, Int)
-type Result = (Input, Int)
-type IntCodeIO = ([Int], [Int])
+type Addr = IM.Key
+type PC = IM.Key
+type RelBase = IM.Key
+type Value = Integer
+type Program = IM.IntMap Value
+type IntCodeIO = ([Value], [Value])
+
+data ProgramState = PS Program PC RelBase
 
 
 --- Break the parameter-mode portion of an instruction into
@@ -22,57 +23,73 @@ type IntCodeIO = ([Int], [Int])
 digits :: Int -> [Int]
 digits = unfoldr (Just . uncurry (flip (,)) . (`divMod` 10))
 
-position :: Program -> Int -> Int
-position p addr = p V.! addr
+position :: Program -> Addr -> Value
+position p addr = p IM.! addr
 
-immediate :: Program -> Int -> Int
-immediate p addr = addr
+immediate :: Program -> Addr -> Value
+immediate p addr = fromIntegral addr
 
-relative :: Program -> Int -> Int -> Int
-relative p relbase addr = p V.! (relbase + addr)
+relative :: Program -> RelBase -> Addr -> Value
+relative p relbase addr = p IM.! (relbase + addr)
 
+
+getValue :: Program -> Addr -> Value
+getValue = (IM.!)
+
+asAddr :: Value -> Addr
+asAddr = fromIntegral
 
 -- Should use ReaderT and WriterT to handle input and output separately...
-eval :: Program -> PC -> RelBase ->  State IntCodeIO (Program, PC, RelBase)
-eval p pc rb = do
+eval :: ProgramState ->  State IntCodeIO ProgramState
+eval (PS p pc rb) = do
      (input, output) <- get
-     let instruction = p V.! pc
+     let instruction = getValue p pc
          (pmodes, opcode) = instruction `divMod` 100
-         pmodeList = digits pmodes
+         pmodeList = digits (fromIntegral pmodes)
      if opcode == 99
-     then return (p, pc, rb)  -- We're done; don't worry about incrementing the pc
-     else let a1 = p V.! (pc + 1)  -- The three arguments
-              a2 = p V.! (pc + 2)
-              a3 = p V.! (pc + 3)
-              getParameter b = case pmodeList !! b of
+     then return (PS p pc rb)  -- We're done; don't worry about incrementing the pc
+     else let a1 = p IM.! (pc + 1)  -- The three arguments
+              a2 = p IM.! (pc + 2)
+              a3 = p IM.! (pc + 3)
+              a1addr = asAddr a1
+              a2addr = asAddr a2
+              a3addr = asAddr a3
+              getParameter :: Int -> Addr -> Value
+              getParameter b = let pmode = fromIntegral $ pmodeList !! b
+                               in case pmode of
                                     0 -> position p
                                     1 -> immediate p
                                     2 -> relative p rb
-              op1 = getParameter 0 a1
-              op2 = getParameter 1 a2
-              doAdd = eval (p V.// [(a3, op1 + op2)]) (pc + 4) rb
-              doMul = eval (p V.// [(a3, op1 * op2)]) (pc + 4) rb
-              doInp x = eval (p V.// [(a1, x)]) (pc + 2) rb
-              doOut = eval p (pc + 2) rb
-              doTrueJump = eval p (bool (pc + 3) op2 (op1 /= 0)) rb
-              doFalseJump = eval p (flip bool (pc + 3) op2 (op1 /= 0)) rb
+                                    otherwise -> error $ "Unknown parameter mode " ++ show pmode
+              op1 = getParameter 0 a1addr
+              op2 = getParameter 1 a2addr
+              doAdd = eval $ PS (IM.insert a3addr (op1 + op2) p) (pc + 4) rb
+              doMul = eval $ PS (IM.insert a3addr (op1 * op2) p) (pc + 4) rb
+              doInp x = eval $ PS (IM.insert a1addr x p) (pc + 2) rb
+              doOut = eval $ PS p (pc + 2) rb
+              doJump tst = eval $ PS p (bool (pc + 3) (asAddr op2) (tst op1)) rb
+              doTest tst = let p' = IM.insert a3addr (fromIntegral $ fromEnum (tst op1 op2)) p
+                           in eval $ PS p' (pc + 4) rb
           in do case opcode of
                   1 -> doAdd
                   2 -> doMul
                   3 -> do let (x:xs) = input
                           put (xs, output) 
                           doInp x
-                  4 -> put (input, output ++ [getParameter 0 a1]) >> doOut
-                  5 -> doTrueJump
-                  6 -> doFalseJump
-                  7 -> eval (p V.// [(a3, fromEnum (op1 < op2))]) (pc + 4) rb
-                  8 -> eval (p V.// [(a3, fromEnum (op1 == op2))]) (pc + 4) rb
-                  9 -> eval p (pc + 2) (rb + op1)
+                  4 -> do put (input, output ++ [getParameter 0 a1addr])
+                          doOut
+                  5 -> doJump (/= 0)
+                  6 -> doJump (== 0)
+                  7 -> doTest (<)
+                  8 -> doTest (==) 
+                  9 -> eval $ PS p (pc + 2) (rb + fromIntegral op1)
 
 getData :: FilePath -> IO Program
 getData fname = do
     contents <- readFile fname
-    return $ V.fromList $ map read $ splitOn "," contents
+    let bytecodes :: [Integer]
+        bytecodes = Prelude.map read $ splitOn "," contents
+    return $ IM.fromList (zip [0..] bytecodes)
 
 
 main = do
@@ -80,4 +97,6 @@ main = do
   program <- getData fname
   let input = [1]
       output = []
-  print $ execState (eval program 0 0) (input, output)
+  -- 203 is too low. I'm getting output of [203, 0], though, which I think means that the opcode 203 isn't working
+  -- not that the BOOST code is 203
+  print $ execState (eval $ PS program 0 0) (input, output)
